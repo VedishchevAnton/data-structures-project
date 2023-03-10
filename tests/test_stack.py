67006f8d6f680ea83e489edf066c7dbdcc91b268
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-10 14:37:23 +0300

    add test_init_Stack

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+from src.stack import Stack
+import unittest
+
+
+class TestStack(unittest.TestCase):
+    """
+    Тесты для класса Stack.
+    """
+    def test_init(self):
+        """
+        Тест конструктора класса Stack.
+        """
+        stack = Stack()
+        self.assertIsNone(stack.top)
