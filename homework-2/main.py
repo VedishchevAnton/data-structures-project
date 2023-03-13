2ecea67dad2636111ff7580f09313083c0c3ec3b
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-13 19:31:28 +0300

    исправлен main.py

--- homework-2/main.py
+++ homework-2/main.py
@@ -4,27 +4,20 @@ if __name__ == '__main__':
     stack = Stack()
     stack.push('data1')
     data = stack.pop()
-    print(data)
 
     # стэк стал пустой
     assert stack.top is None
-    print(stack.top)
 
     # pop() удаляет элемент и возвращает данные удаленного элемента
     assert data == 'data1'
-    print(data)
 
     stack = Stack()
     stack.push('data1')
     stack.push('data2')
     data = stack.pop()
-    print(data)
 
     # теперь последний элемента содержит данные data1
     assert stack.top.data == 'data1'
-    print(stack.top.data)
 
     # данные удаленного элемента
     assert data == 'data2'
-    print(data)
-
