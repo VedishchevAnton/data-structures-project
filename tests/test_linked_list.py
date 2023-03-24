e94ae361a1eab446174243e04877a430d16d1a7b
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-24 20:50:44 +0300

    add test_linked_list.py and test_init

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+from src.linked_list import LinkedList
+
+
+class LinkedListTest(unittest.TestCase):
+
+    def test_init(self):
+        """
+        Тест конструктора класса Queue
+        """
+        linked_list = LinkedList()
+        self.assertEqual(linked_list.head, None)  # проверка начала очереди
+        self.assertEqual(linked_list.tail, None)  # проверка конца очереди
