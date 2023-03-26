3381bd7a83fe81a90b96ef278abc9d6fca0d0df4
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-26 22:48:27 +0300

    add test__str_

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -3,16 +3,6 @@ import unittest
 from src.linked_list import LinkedList
 
 
-#
-def test__str__():
-    linked_list = LinkedList()
-    assert str(linked_list) == "None"
-    linked_list.insert_beginning({'id_test': 1})
-    assert str(linked_list) == " {'id_test': 1} -> None"
-    linked_list.insert_beginning({'id_test': 2})
-    assert str(linked_list) == " {'id_test': 2} -> {'id_test': 1} -> None"
-
-
 class LinkedListTest(unittest.TestCase):
 
     def test_init(self):
@@ -39,10 +29,10 @@ class LinkedListTest(unittest.TestCase):
         linked_list.insert_at_end({'id_test': 2})
         self.assertEqual(linked_list.tail.data, {'id_test': 2})
 
-    # def test__str__(self):
-    #     linked_list = LinkedList()
-    #     self.assertEqual(str(linked_list), 'None')
-    #     linked_list.insert_beginning({'id_test': 1})
-    #     self.assertEqual(str(linked_list), f" {'id_test': 1}->None")
-    #     linked_list.insert_beginning({'id_test': 2})
-    #     self.assertEqual(str(linked_list), f" {'id_test': 2}-> {'id_test': 1} -> None")
+    def test__str__(self):
+        linked_list = LinkedList()
+        self.assertEqual(str(linked_list), 'None')
+        linked_list.insert_beginning({'id_test': 1})
+        self.assertEqual(str(linked_list), f"{'id_test': 1} -> None")
+        linked_list.insert_beginning({'id_test': 2})
+        self.assertEqual(str(linked_list), f"{'id_test': 2} -> {'id_test': 1} -> None")
